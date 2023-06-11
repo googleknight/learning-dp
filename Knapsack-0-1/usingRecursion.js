5aed43ddce959cc5c101ef2943e78ab4170844a8
Shubham Mathur <Shubham_Mathur@mckinsey.com>
2023-06-11 19:29:45 +0530

    Solved knapsack-0/1 using recursion.

--- Knapsack-0-1/usingRecursion.js
+++ Knapsack-0-1/usingRecursion.js
@@ -1,7 +1,16 @@
+// const w=10,wt=[1,3,4,6], val=[20,30,10,50],n=4;
 const usingRecursion = (wt,val,w,n) => {
-    console.log("🚀 ~ file: usingRecursion.js:2 ~ usingRecursion ~ wt,val,w,n:", wt,val,w,n)
-
-return 0;
+    if(w===0||n===0)
+        return 0;
+    if(wt[n-1]<=w){
+        let taken = val[n-1]+usingRecursion(wt,val,w-wt[n-1],n-1);
+        let notTaken = usingRecursion(wt,val,w,n-1);
+        return Math.max(taken,notTaken);
+    }
+    if(wt[n-1]>w){
+        return usingRecursion(wt,val,w,n-1);
+    }
+    
 }
 
 export default usingRecursion
