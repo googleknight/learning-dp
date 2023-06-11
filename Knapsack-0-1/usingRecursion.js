2170e29277bf97e61c7179bafceedd9b1fdffbb8
Shubham Mathur <Shubham_Mathur@mckinsey.com>
2023-06-11 19:23:49 +0530

    Basic setup.

---
+++ Knapsack-0-1/usingRecursion.js
@@ -0,0 +1,7 @@
+const usingRecursion = (wt,val,w,n) => {
+    console.log("🚀 ~ file: usingRecursion.js:2 ~ usingRecursion ~ wt,val,w,n:", wt,val,w,n)
+
+return 0;
+}
+
+export default usingRecursion
---
+++ index.js
@@ -0,0 +1,3 @@
+import usingRecursion from "./Knapsack-0-1/usingRecursion.js"
+const w=10,wt=[1,3,4,6], val=[20,30,10,50],n=4;
+console.log(usingRecursion(wt,val,w,n))
